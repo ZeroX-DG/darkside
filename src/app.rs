831c18d1c9606eeb520d1c45109b54f6cc56b084
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2019-07-05 22:38:19 +1200

    added get term size and utf-8 support

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,8 @@
 use ncurses::*;
 
 pub fn new_app() {
+  let locale_conf = LcCategory::all;
+  setlocale(locale_conf, "en_US.UTF-8");
   initscr();
   noecho();
   keypad(stdscr(), true);
@@ -13,4 +15,11 @@ pub fn wait_for_key() -> i32 {
 
 pub fn translate_key(key: char) -> i32 {
   key as i32
+}
+
+pub fn get_term_size() -> (i32, i32) {
+  let mut x = 0;
+  let mut y = 0;
+  getmaxyx(stdscr(), &mut y, &mut x);
+  (x, y)
 }
